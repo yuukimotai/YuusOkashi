83a5d0c96916bd61a460c0c37d5538a16d35e691
yuukimotai <yuus.saddharma@gmail.com>
2022-11-16 18:16:55 +0900

    Initial Commit

---
+++ YuusOkashi/YuusOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YuusOkashiApp.swift
+//  YuusOkashi
+//
+//  Created by 茂田井雄輝 on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct YuusOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
